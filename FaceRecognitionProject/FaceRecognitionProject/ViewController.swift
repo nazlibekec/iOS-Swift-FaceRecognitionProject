56e66f271e5be1d2a5ed74b3c6d5bdd4f83693f3
nazlibekec <nazlibekec@gmail.com>
2023-02-16 00:49:42 +0300

    Finish

--- FaceRecognitionProject/FaceRecognitionProject/ViewController.swift
+++ FaceRecognitionProject/FaceRecognitionProject/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInClicked(_ sender: Any) {
+        
+        let authContext = LAContext()
+        var error : NSError?
+        
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Is it you?") { success, error in
+                
+                if success == true {
+                    //successful auth
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "toSecondVC", sender: nil)
+                    }
+                    
+                } else {
+                    DispatchQueue.main.async {
+                        self.textLabel.text = "Error!"
+                    }
+                }
+            }
+        }
+    }
 }
 
+//infodan izin iste.
+// UIViewController.performSegue(withIdentifier:sender:) must be used from main thread only.  Bu hatayı aldık. Çözümü: ana thread de çalıştırmamız gerek. DispatchQueue ile.
