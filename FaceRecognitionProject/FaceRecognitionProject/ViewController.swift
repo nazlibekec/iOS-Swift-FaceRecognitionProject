5ecfaad9532660c1a86789862cacf25d550e5b53
nazlibekec <nazlibekec@gmail.com>
2023-02-16 00:22:35 +0300

    First Commit

---
+++ FaceRecognitionProject/FaceRecognitionProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceRecognitionProject
+//
+//  Created by Nazlı Bekeç on 16.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
